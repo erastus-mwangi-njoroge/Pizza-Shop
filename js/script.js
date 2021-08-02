37f7e1f68a80f5684611218118158b610b9be1d6
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-08-02 15:15:10 +0300

    add a function that gets input from the form and give an output

--- js/script.js
+++ js/script.js
@@ -1,2 +1,45 @@
 var pizzaPrice = [1000, 800, 500]
 var transport = [0, 500];
+$(document).ready(function () {
+    $('form#orderForm').submit(function (event) {
+        event.preventDefault();
+        
+        var name = $('#name').val();
+
+        var contact = parseInt($('#contact').val());
+
+
+        var pizzaSize = parseInt($('#size').val());
+
+        var pizzaCrust = $('#crust').val();
+
+        var pizzaTopping = $('#topping').val();
+
+        var pizzaQuantity = parseInt($('#quantity').val());
+
+        var pizzaDeliver = parseInt($('#deliver').val());
+
+
+        var price = pizzaPrice[pizzaSize - 1];
+
+
+        var DeliveryCost = transport[pizzaDeliver - 1];
+
+
+
+        newOrder = new Order(pizzaSize, pizzaCrust, pizzaTopping);
+        newTotal = new Total(price, pizzaQuantity, DeliveryCost);
+        if (pizzaDeliver===1){
+        alert("Hello " + name)
+        alert("Your oder contains" + newOrder.fullOrder() + ".continue to see your total bill");
+        alert("your bill is: " + newTotal.finalTotal() + ".You will receive your pizza in the next few minutes");
+        }else{
+            if(pizzaDeliver===2){
+                prompt("Hello " + name + "Please enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your Pizza oder contains" + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal() + ".You pizza will be delevered in the next 30minutes");
+            }
+        }
+
+    });
